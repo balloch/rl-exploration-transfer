389d2c22492fa2fe1ad30050d7daede8fe284624
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-02-06 11:13:23 -0500

    move noisy layer and mlps to separate file and added noisy actor

---
+++ rlexplore/noisy_nets/noisy_layer.py
@@ -0,0 +1,67 @@
+class NoisyLinear(nn.Module):
+
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, std_init: float = 0.5):
+        super().__init__()
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.std_init = std_init
+        self.bias = bias
+
+        self.weight_mu = nn.Parameter(torch.Tensor(self.out_features, self.in_features))
+        self.weight_sigma = nn.Parameter(torch.Tensor(self.out_features, self.in_features))
+
+        self.register_buffer(
+            "weight_epsilon", torch.Tensor(self.out_features, self.in_features)
+        )
+
+        if self.bias:
+            self.bias_mu = nn.Parameter(torch.Tensor(self.out_features))
+            self.bias_sigma = nn.Parameter(torch.Tensor(self.out_features))
+            self.register_buffer(
+                "bias_epsilon", torch.Tensor(self.out_features)
+            )
+
+        self.reset_parameters()
+        self.reset_noise()
+
+    def reset_parameters(self):
+        mu_range = 1 / np.sqrt(self.in_features)
+        self.weight_mu.data.uniform_(-mu_range, mu_range)
+        self.weight_sigma.data.fill_(
+            self.std_init / np.sqrt(self.in_features)
+        )
+
+        if self.bias:
+            self.bias_mu.data.uniform_(-mu_range, mu_range)
+            self.bias_sigma.data.fill_(
+                self.std_init / np.sqrt(self.out_features)
+            )
+
+    def reset_noise(self):
+        epsilon_in = NoisyLinear.scale_noise(self.in_features)
+        epsilon_out = NoisyLinear.scale_noise(self.out_features)
+
+        self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+        if self.bias:
+            self.bias_epsilon.copy_(epsilon_out)
+
+    def forward(self, x: torch.Tensor, train: bool = True) -> torch.Tensor:
+        if train:
+            return F.linear(
+                x,
+                self.weight_mu + self.weight_sigma * self.weight_epsilon,
+                self.bias_mu + self.bias_sigma * self.bias_epsilon if self.bias else 0,
+            )
+        else:
+            return F.linear(
+                x,
+                self.weight_mu,
+                self.bias_mu if self.bias else 0,
+            )
+        
+    @staticmethod
+    def scale_noise(size: int) -> torch.Tensor:
+        x = torch.randn(size)
+
+        return x.sign().mul(x.abs().sqrt())
---
+++ rlexplore/noisy_nets/noisy_mlp.py
@@ -0,0 +1,297 @@
+from typing import Union, List, Type, Tuple, Dict, Optional, Any
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.utils import get_device
+from stable_baselines3.common.type_aliases import Schedule
+from stable_baselines3.common.torch_layers import (
+    BaseFeaturesExtractor,
+    NatureCNN,
+    FlattenExtractor,
+)
+
+
+import gym
+
+import numpy as np
+
+
+class NoisyLinear(nn.Module):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        bias: bool = True,
+        std_init: float = 0.5,
+    ):
+        super().__init__()
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.std_init = std_init
+        self.bias = bias
+
+        self.weight_mu = nn.Parameter(torch.Tensor(self.out_features, self.in_features))
+        self.weight_sigma = nn.Parameter(
+            torch.Tensor(self.out_features, self.in_features)
+        )
+
+        self.register_buffer(
+            "weight_epsilon", torch.Tensor(self.out_features, self.in_features)
+        )
+
+        if self.bias:
+            self.bias_mu = nn.Parameter(torch.Tensor(self.out_features))
+            self.bias_sigma = nn.Parameter(torch.Tensor(self.out_features))
+            self.register_buffer("bias_epsilon", torch.Tensor(self.out_features))
+
+        self.reset_parameters()
+        self.reset_noise()
+
+    def reset_parameters(self):
+        mu_range = 1 / np.sqrt(self.in_features)
+        self.weight_mu.data.uniform_(-mu_range, mu_range)
+        self.weight_sigma.data.fill_(self.std_init / np.sqrt(self.in_features))
+
+        if self.bias:
+            self.bias_mu.data.uniform_(-mu_range, mu_range)
+            self.bias_sigma.data.fill_(self.std_init / np.sqrt(self.out_features))
+
+    def reset_noise(self):
+        epsilon_in = NoisyLinear.scale_noise(self.in_features)
+        epsilon_out = NoisyLinear.scale_noise(self.out_features)
+
+        self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+        if self.bias:
+            self.bias_epsilon.copy_(epsilon_out)
+
+    def forward(self, x: torch.Tensor, train: bool = True) -> torch.Tensor:
+        if train:
+            return F.linear(
+                x,
+                self.weight_mu + self.weight_sigma * self.weight_epsilon,
+                self.bias_mu + self.bias_sigma * self.bias_epsilon if self.bias else 0,
+            )
+        else:
+            return F.linear(
+                x,
+                self.weight_mu,
+                self.bias_mu if self.bias else 0,
+            )
+
+    @staticmethod
+    def scale_noise(size: int) -> torch.Tensor:
+        x = torch.randn(size)
+
+        return x.sign().mul(x.abs().sqrt())
+
+
+def create_noisy_mlp(
+    input_dim: int,
+    output_dim: int,
+    net_arch: List[int],
+    activation_fn: Type[nn.Module] = nn.ReLU,
+    squash_output: bool = False,
+    with_bias: bool = True,
+) -> List[nn.Module]:
+
+    if len(net_arch) > 0:
+        modules = [nn.Linear(input_dim, net_arch[0], bias=with_bias), activation_fn()]
+    else:
+        modules = []
+
+    for idx in range(len(net_arch) - 1):
+        if idx == len(net_arch) - 2:
+            modules.append(
+                NoisyLinear(net_arch[idx], net_arch[idx + 1], bias=with_bias)
+            )
+        else:
+            modules.append(nn.Linear(net_arch[idx], net_arch[idx + 1], bias=with_bias))
+        modules.append(activation_fn())
+
+    if output_dim > 0:
+        last_layer_dim = net_arch[-1] if len(net_arch) > 0 else input_dim
+        modules.append(NoisyLinear(last_layer_dim, output_dim, bias=with_bias))
+
+    if squash_output:
+        modules.append(nn.Tanh())
+
+    return modules
+
+
+class NoisyMlpExtractor(nn.Module):
+    """
+    Constructs an MLP that receives the output from a previous features extractor (i.e. a CNN) or directly
+    the observations (if no features extractor is applied) as an input and outputs a latent representation
+    for the policy and a value network.
+    The ``net_arch`` parameter allows to specify the amount and size of the hidden layers.
+    It can be in either of the following forms:
+    1. ``dict(vf=[<list of layer sizes>], pi=[<list of layer sizes>])``: to specify the amount and size of the layers in the
+        policy and value nets individually. If it is missing any of the keys (pi or vf),
+        zero layers will be considered for that key.
+    2. ``[<list of layer sizes>]``: "shortcut" in case the amount and size of the layers
+        in the policy and value nets are the same. Same as ``dict(vf=int_list, pi=int_list)``
+        where int_list is the same for the actor and critic.
+    .. note::
+        If a key is not specified or an empty list is passed ``[]``, a linear network will be used.
+    :param feature_dim: Dimension of the feature vector (can be the output of a CNN)
+    :param net_arch: The specification of the policy and value networks.
+        See above for details on its formatting.
+    :param activation_fn: The activation function to use for the networks.
+    :param device: PyTorch device.
+    """
+
+    def __init__(
+        self,
+        feature_dim: int,
+        net_arch: Union[List[int], Dict[str, List[int]]],
+        activation_fn: Type[nn.Module],
+        device: Union[torch.device, str] = "auto",
+    ) -> None:
+        super().__init__()
+        device = get_device(device)
+        policy_net: List[nn.Module] = []
+        value_net: List[nn.Module] = []
+        last_layer_dim_pi = feature_dim
+        last_layer_dim_vf = feature_dim
+
+        # save dimensions of layers in policy and value nets
+        if isinstance(net_arch, dict):
+            # Note: if key is not specificed, assume linear network
+            pi_layers_dims = net_arch.get("pi", [])  # Layer sizes of the policy network
+            vf_layers_dims = net_arch.get("vf", [])  # Layer sizes of the value network
+        else:
+            pi_layers_dims = vf_layers_dims = net_arch
+
+        # Iterate through the policy layers and build the policy net
+        for i in range(len(pi_layers_dims)):
+            curr_layer_dim = pi_layers_dims[i]
+            if i < len(pi_layers_dims) - 2:
+                policy_net.append(nn.Linear(last_layer_dim_pi, curr_layer_dim))
+            else:
+                policy_net.append(NoisyLinear(last_layer_dim_pi, curr_layer_dim))
+            policy_net.append(activation_fn())
+            last_layer_dim_pi = curr_layer_dim
+
+        # Iterate through the value layers and build the value net
+        for curr_layer_dim in vf_layers_dims:
+            value_net.append(nn.Linear(last_layer_dim_vf, curr_layer_dim))
+            value_net.append(activation_fn())
+            last_layer_dim_vf = curr_layer_dim
+
+        # Save dim, used to create the distributions
+        self.latent_dim_pi = last_layer_dim_pi
+        self.latent_dim_vf = last_layer_dim_vf
+
+        # Create networks
+        # If the list of layers is empty, the network will just act as an Identity module
+        self.policy_net = nn.Sequential(*policy_net).to(device)
+        self.value_net = nn.Sequential(*value_net).to(device)
+
+    def forward(self, features: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        :return: latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+        """
+        return self.forward_actor(features), self.forward_critic(features)
+
+    def forward_actor(self, features: torch.Tensor) -> torch.Tensor:
+        return self.policy_net(features)
+
+    def forward_critic(self, features: torch.Tensor) -> torch.Tensor:
+        return self.value_net(features)
+
+
+class NoisyActorCriticPolicy(ActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule: Schedule,
+        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
+        activation_fn: Type[nn.Module] = nn.Tanh,
+        ortho_init: bool = True,
+        use_sde: bool = False,
+        log_std_init: float = 0.0,
+        full_std: bool = True,
+        sde_net_arch: Optional[List[int]] = None,
+        use_expln: bool = False,
+        squash_output: bool = False,
+        features_extractor_class: Type[BaseFeaturesExtractor] = FlattenExtractor,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        normalize_images: bool = True,
+        optimizer_class: Type[torch.optim.Optimizer] = torch.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        super(NoisyActorCriticPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            ortho_init,
+            use_sde,
+            log_std_init,
+            full_std,
+            sde_net_arch,
+            use_expln,
+            squash_output,
+            features_extractor_class,
+            features_extractor_kwargs,
+            normalize_images,
+            optimizer_class,
+            optimizer_kwargs,
+        )
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = NoisyMlpExtractor(
+            self.features_dim,
+            net_arch=self.net_arch,
+            activation_fn=self.activation_fn,
+            device=self.device,
+        )
+
+
+class NoisyActorCriticCnnPolicy(NoisyActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule: Schedule,
+        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
+        activation_fn: Type[nn.Module] = nn.Tanh,
+        ortho_init: bool = True,
+        use_sde: bool = False,
+        log_std_init: float = 0.0,
+        full_std: bool = True,
+        sde_net_arch: Optional[List[int]] = None,
+        use_expln: bool = False,
+        squash_output: bool = False,
+        features_extractor_class: Type[BaseFeaturesExtractor] = NatureCNN,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        normalize_images: bool = True,
+        optimizer_class: Type[torch.optim.Optimizer] = torch.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+    ):
+        super().__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            ortho_init,
+            use_sde,
+            log_std_init,
+            full_std,
+            sde_net_arch,
+            use_expln,
+            squash_output,
+            features_extractor_class,
+            features_extractor_kwargs,
+            normalize_images,
+            optimizer_class,
+            optimizer_kwargs,
+        )
