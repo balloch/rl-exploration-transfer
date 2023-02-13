83307ded6603b702c05511cb2858cbed946655e0
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-02-13 00:35:35 -0500

    running noisy layers as last layers (including action head)

--- rlexplore/noisy_nets/noisy_actor.py
+++ rlexplore/noisy_nets/noisy_actor.py
@@ -10,11 +10,19 @@ from stable_baselines3.common.torch_layers import (
     NatureCNN,
     FlattenExtractor,
 )
+from stable_baselines3.common.distributions import CategoricalDistribution
 
 
 import gym
 
 from .noisy_mlp import NoisyMlpExtractor
+from .noisy_layer import NoisyLinear
+
+
+class NoisyNetCategoricalDistribution(CategoricalDistribution):
+    def proba_distribution_net(self, latent_dim: int) -> nn.Module:
+        action_logits = NoisyLinear(latent_dim, self.action_dim)
+        return action_logits
 
 
 class NoisyActorCriticPolicy(ActorCriticPolicy):
@@ -37,7 +45,10 @@ class NoisyActorCriticPolicy(ActorCriticPolicy):
         normalize_images: bool = True,
         optimizer_class: Type[torch.optim.Optimizer] = torch.optim.Adam,
         optimizer_kwargs: Optional[Dict[str, Any]] = None,
+        num_noisy_layers: int = 2,
     ):
+        self.num_noisy_layers = num_noisy_layers
+
         super(NoisyActorCriticPolicy, self).__init__(
             observation_space,
             action_space,
@@ -58,23 +69,30 @@ class NoisyActorCriticPolicy(ActorCriticPolicy):
             optimizer_kwargs,
         )
 
+    def _build(self, lr_schedule: Schedule) -> None:
+        if self.num_noisy_layers > 0 and isinstance(
+            self.action_dist, CategoricalDistribution
+        ):
+            self.action_dist = NoisyNetCategoricalDistribution(self.action_space.n)
+        return super()._build(lr_schedule)
+
     def _build_mlp_extractor(self) -> None:
         self.mlp_extractor = NoisyMlpExtractor(
             self.features_dim,
             net_arch=self.net_arch,
             activation_fn=self.activation_fn,
             device=self.device,
+            num_noisy_layers=self.num_noisy_layers - 1,
         )
 
 
-# TODO: move noisy layers to the action policy instead of the MLP feature extractor
 class NoisyActorCriticCnnPolicy(NoisyActorCriticPolicy):
     def __init__(
         self,
         observation_space: gym.spaces.Space,
         action_space: gym.spaces.Space,
         lr_schedule: Schedule,
-        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = None,
+        net_arch: Optional[List[Union[int, Dict[str, List[int]]]]] = [64],
         activation_fn: Type[nn.Module] = nn.Tanh,
         ortho_init: bool = True,
         use_sde: bool = False,
@@ -88,6 +106,7 @@ class NoisyActorCriticCnnPolicy(NoisyActorCriticPolicy):
         normalize_images: bool = True,
         optimizer_class: Type[torch.optim.Optimizer] = torch.optim.Adam,
         optimizer_kwargs: Optional[Dict[str, Any]] = None,
+        num_noisy_layers: int = 2,
     ):
         super().__init__(
             observation_space,
@@ -107,4 +126,5 @@ class NoisyActorCriticCnnPolicy(NoisyActorCriticPolicy):
             normalize_images,
             optimizer_class,
             optimizer_kwargs,
+            num_noisy_layers,
         )
