e349e666f59d3498d71d02cb6d6c0a6280a3ddcf
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-04-02 23:58:12 -0400

    wrote ddpg version of poly rl

---
+++ examples/poly_rl_ddpg_mountain_car.py
@@ -0,0 +1,121 @@
+import os
+import sys
+import numpy as np
+
+curren_dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(curren_dir_path, os.pardir))
+sys.path.append(parent_dir_path)
+
+import argparse
+import gym
+from stable_baselines3 import DDPG
+from stable_baselines3.common.noise import NormalActionNoise
+
+import time
+
+from rlexplore.poly_rl.poly_rl_policy import PolyRLDDPGPolicy, DDPGPolicy
+
+LOAD_MODEL = False
+TIMESTEPS = 1000000
+# ENV_NAME = "MountainCarContinuous-v0"
+ENV_NAME = "Pendulum-v1"
+
+
+def get_args():
+    str2bool = lambda s: s.lower() in ["true", "1", "t", "y", "yes", "yeah"]
+    parser = argparse.ArgumentParser(description="RL")
+    parser.add_argument("--algo", type=str, default="ppo")
+    parser.add_argument("--env-id", type=str, default=ENV_NAME)
+    parser.add_argument("--total-time-steps", type=int, default=TIMESTEPS)
+    parser.add_argument("--n-steps", type=int, default=128)
+
+    parser.add_argument("--load-model", type=str2bool, default=LOAD_MODEL)
+
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--poly-rl", type=str2bool, default=True)
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    timestamp = int(time.time())
+
+    model_name = f"{args.env_id.lower().replace('-', '_')}_polyrl_cnn_ppo_{args.total_time_steps}_{timestamp}"
+    model_file_path = f"models/{model_name}"
+    log_path = f"logs/{model_name}"
+
+    env = gym.make(args.env_id)
+
+    num_episodes = int(args.total_time_steps / args.n_steps)
+
+    n_actions = env.action_space.shape[-1]
+    action_noise = NormalActionNoise(
+        mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions)
+    )
+
+    model = DDPG(
+        policy=PolyRLDDPGPolicy if args.poly_rl else DDPGPolicy,
+        env=env,
+        verbose=1,
+        action_noise=action_noise if not args.poly_rl else None,
+        # learning_rate=0.001,
+        # n_steps=2048,
+        # batch_size=256,
+        # n_epochs=4,
+        gamma=args.gamma,
+        # gae_lambda=0.95,
+        # clip_range=0.2,
+        # ent_coef=0 if args.poly_rl else 0.01,
+        # vf_coef=0.5,
+        # max_grad_norm=0.5,
+        tensorboard_log="./logs/",
+        policy_kwargs=dict(logdir=log_path) if args.poly_rl else dict(),
+    )
+
+    def test_model():
+        total_reward = 0
+        eps = 1
+        num_steps = 1000
+
+        observation = env.reset()
+        for _ in range(num_steps):
+            action, _ = model.predict(observation, deterministic=True)
+            action = np.array(action)
+            if action.shape == ():
+                action = action[np.newaxis]
+            # print(action.shape, action)
+            # sys.exit()
+            observation, reward, done, _ = env.step(action)
+            total_reward += reward
+
+            if done:
+                observation = env.reset()
+                eps += 1
+
+        print("-------------------------------------------")
+        print(f"Average Rewards: {total_reward / eps}")
+        print(f"Average Episode Length: {num_steps / eps}")
+        print(f"Total Reward: {total_reward}")
+        print(f"Num Episodes: {eps}")
+        print("-------------------------------------------")
+
+    test_model()
+
+    if args.load_model:
+        model.load(model_file_path)
+    else:
+        model.learn(
+            total_timesteps=args.total_time_steps,
+            log_interval=1,
+            tb_log_name=model_name,
+        )
+
+    test_model()
+
+    if not args.load_model:
+        model.save(model_file_path)
+
+    env.close()
--- rlexplore/poly_rl/poly_rl_policy.py
+++ rlexplore/poly_rl/poly_rl_policy.py
@@ -6,6 +6,7 @@ import gym
 import numpy as np
 
 from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.ddpg.policies import MlpPolicy as DDPGPolicy
 from stable_baselines3.common.type_aliases import Schedule
 from stable_baselines3.common.torch_layers import (
     BaseFeaturesExtractor,
@@ -95,11 +96,11 @@ class PolyRLActorCriticPolicy(ActorCriticPolicy):
         self, observation: torch.Tensor, deterministic: bool = False
     ) -> torch.Tensor:
         self.counter_actions += 1
+        action = None
         if self.start_steps < self.counter_actions:
             action = super()._predict(
                 observation=observation, deterministic=deterministic
             )
-            return action
         else:
             state = np.array(observation.cpu())
             self.previous_state = state
@@ -111,7 +112,10 @@ class PolyRLActorCriticPolicy(ActorCriticPolicy):
             )
             action = torch.clamp(action, -1, 1).reshape(-1)
             self.previous_action = action
-            return action
+        action = np.array(action)
+        if action.shape == ():
+            action = action[np.newaxis]
+        return action
 
     def predict(
         self,
@@ -126,7 +130,7 @@ class PolyRLActorCriticPolicy(ActorCriticPolicy):
             self.poly_rl.reset_parameters_in_beginning_of_episode(
                 self.nb_environment_reset
             )
-        return super().predict(observation, state, episode_start, deterministic)
+        return super().predict(observation, state, episode_start, deterministic)        
 
 
 class PolyRLActorCriticCnnPolicy(PolyRLActorCriticPolicy):
@@ -180,4 +184,158 @@ class PolyRLActorCriticCnnPolicy(PolyRLActorCriticPolicy):
             lambda_,
             gamma,
             logdir,
+        )
+
+
+class PolyRLDDPGPolicy(DDPGPolicy):
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule: Schedule,
+        net_arch: Optional[Union[List[int], Dict[str, List[int]]]] = None,
+        activation_fn: Type[nn.Module] = nn.ReLU,
+        features_extractor_class: Type[BaseFeaturesExtractor] = FlattenExtractor,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        normalize_images: bool = True,
+        optimizer_class: Type[torch.optim.Optimizer] = torch.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+        n_critics: int = 2,
+        share_features_extractor: bool = False,
+        betta: float = 0.0001,
+        epsilon: float = 0,
+        sigma_squared: float = 0.00007,
+        lambda_: float = 0.035,
+        gamma: float = 0.99,
+        start_steps: int = 10000,
+        logdir=None,
+    ):
+        super(PolyRLDDPGPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            features_extractor_class,
+            features_extractor_kwargs,
+            normalize_images,
+            optimizer_class,
+            optimizer_kwargs,
+            n_critics,
+            share_features_extractor,
+        )
+        self.betta = (betta,)
+        self.epsilon = (epsilon,)
+        self.sigma_squared = (sigma_squared,)
+        self.lambda_ = lambda_
+        self.start_steps = start_steps
+        self.poly_rl = PolyRL(
+            gamma=gamma,
+            nb_actions=action_space.shape[0],
+            nb_observations=observation_space.shape[0],
+            max_action=float(action_space.high[0]),
+            min_action=float(min(action_space.low)),
+            actor_target_function=self,
+            betta=betta,
+            epsilon=epsilon,
+            sigma_squared=sigma_squared,
+            lambda_=lambda_,
+        )
+        self.counter_actions = 0
+        assert logdir is not None
+        self.writer = SummaryWriter(logdir=logdir)
+        self.writer.STOP = True
+        self.previous_action = None
+
+    def get_exploration_percentage(self):
+        return self.poly_rl.percentage_exploration
+
+    def _predict(
+        self, observation: torch.Tensor, deterministic: bool = False
+    ) -> torch.Tensor:
+        self.counter_actions += 1
+        action = None
+        if self.start_steps < self.counter_actions:
+            action = super()._predict(
+                observation=observation, deterministic=deterministic
+            )
+        else:
+            state = np.array(observation.cpu())
+            self.previous_state = state
+            action = self.poly_rl.select_action(
+                state,
+                self.previous_action,
+                tensor_board_writer=self.writer,
+                step_number=self.counter_actions,
+            )
+        if action.shape == ():
+            action = action[np.newaxis]
+        return action
+
+    def predict(
+        self,
+        observation: Union[np.ndarray, Dict[str, np.ndarray]],
+        state: Optional[Tuple[np.ndarray, ...]] = None,
+        episode_start: bool = False,
+        deterministic: bool = False,
+    ):
+        if episode_start:
+            self.previous_action = None
+            self.previous_state = None
+            self.poly_rl.reset_parameters_in_beginning_of_episode(
+                self.nb_environment_reset
+            )
+        return super().predict(observation, state, episode_start, deterministic)  
+
+
+class PolyRLDDPGCNNPolicy(PolyRLDDPGPolicy):
+    """
+    Policy class (with both actor and critic) for TD3.
+    :param observation_space: Observation space
+    :param action_space: Action space
+    :param lr_schedule: Learning rate schedule (could be constant)
+    :param net_arch: The specification of the policy and value networks.
+    :param activation_fn: Activation function
+    :param features_extractor_class: Features extractor to use.
+    :param features_extractor_kwargs: Keyword arguments
+        to pass to the features extractor.
+    :param normalize_images: Whether to normalize images or not,
+         dividing by 255.0 (True by default)
+    :param optimizer_class: The optimizer to use,
+        ``th.optim.Adam`` by default
+    :param optimizer_kwargs: Additional keyword arguments,
+        excluding the learning rate, to pass to the optimizer
+    :param n_critics: Number of critic networks to create.
+    :param share_features_extractor: Whether to share or not the features extractor
+        between the actor and the critic (this saves computation time)
+    """
+
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule: Schedule,
+        net_arch: Optional[Union[List[int], Dict[str, List[int]]]] = None,
+        activation_fn: Type[nn.Module] = nn.ReLU,
+        features_extractor_class: Type[BaseFeaturesExtractor] = NatureCNN,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        normalize_images: bool = True,
+        optimizer_class: Type[torch.optim.Optimizer] = torch.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+        n_critics: int = 2,
+        share_features_extractor: bool = False,
+    ):
+        super().__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            features_extractor_class,
+            features_extractor_kwargs,
+            normalize_images,
+            optimizer_class,
+            optimizer_kwargs,
+            n_critics,
+            share_features_extractor,
         )
