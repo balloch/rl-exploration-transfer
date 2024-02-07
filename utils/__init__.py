3e8c06f18ca1bfb87d0d39bffdf8afb39f78c01e
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-06 19:43:57 -0500

    modified argument library

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,17 @@
+import inspect
+
+
 def default(val, d, null_val=None):
     return val if val != null_val else d
+
+
+def get_all_subclasses_from_modules(*ms, super_cls=None, lower_case_keys=True):
+    return {
+        (k.lower() if lower_case_keys else k): v
+        for m in ms
+        for k, v in inspect.getmembers(
+            m,
+            lambda obj: inspect.isclass(obj)
+            and (super_cls is None or issubclass(obj, super_cls)),
+        )
+    }
--- utils/arg_types.py
+++ utils/arg_types.py
@@ -1,4 +1,5 @@
 import json
+from utils import get_all_subclasses_from_modules
 
 str2bool = lambda s: s.lower() in [
     "true",
@@ -26,8 +27,10 @@ def tup(t):
     return f
 
 
-def module_enum(*mods):
-    mod_dict = {k.lower(): v for mod in reversed(mods) for k, v in vars(mod).items()}
+def module_enum(*mods, super_cls_filter=None):
+    mod_dict = get_all_subclasses_from_modules(
+        *mods, super_cls=super_cls_filter, lower_case_keys=True
+    )
     f = lambda s: mod_dict[s.lower()]
     joiner = f".\\*/"
     f.__name__ = f"{joiner.join([mod.__name__ for mod in mods])}.\\*"
