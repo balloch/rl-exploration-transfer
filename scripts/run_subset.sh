e85499bee95f3f921a2ab86fbc8cdc1ab6459d09
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-08 01:27:38 -0500

    added replace rather than suffix removal

--- scripts/run_subset.sh
+++ scripts/run_subset.sh
@@ -3,6 +3,6 @@ methods=$(echo $1 | tr "," "\n")
 for var in $methods
 do
     command=$(cat scripts/run_all_experiments.sh | grep "$var")
-    command=${command%\$@}
-    eval "$command ${@:2}"
+    command="${command//\$@/"${@:2}"}" 
+    eval "$command"
 done
