8a22736a919179aa872d79d833f153d4a9d93b87
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-16 14:10:48 -0500

    minor bug fix

--- rlexplore/diayn/wrapper.py
+++ rlexplore/diayn/wrapper.py
@@ -7,13 +7,13 @@ class DiaynSkillWrapper(gym.ObservationWrapper):
 
     def __init__(self, env: gym.Env, skill_key="skill", state_key="state"):
         super().__init__(env)
-        self.skill_key = skill_key
         self.state_key = state_key
+        self.skill_key = skill_key
         self.state_space = self.observation_space
-        self.skill_space = gym.spaces.Box(0, 1, 5, np.float32)
+        self.skill_space = gym.spaces.Box(0, 1, (5, ), np.float32)
         self.observation_space = gym.spaces.Dict({
-            self.skill_key: self.state_space,
-            self.state_key: self.skill_space
+            self.state_key: self.state_space,
+            self.skill_key: self.skill_space
         })
 
     def sample_skill(self):
@@ -21,7 +21,7 @@ class DiaynSkillWrapper(gym.ObservationWrapper):
 
     def observation(self, observation: Any) -> Any:
         return {
-            self.skill_key: observation,
-            self.state_key: self.sample_skill()
+            self.state_key: observation,
+            self.skill_key: self.sample_skill()
         }
     
