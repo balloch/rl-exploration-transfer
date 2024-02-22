348cbbbc1ed5a3931d2e821a5a826a0bf986c8e6
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-21 19:16:19 -0500

    move ppo params to only ppo

--- scripts/run_all_experiments.sh
+++ scripts/run_all_experiments.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 ./scripts/run_experiment.sh ppo.yml base.yml $@ --experiment-suffix _none
-./scripts/run_experiment.sh re3.yml ir_ppo.yml base.yml $@
-./scripts/run_experiment.sh rise.yml ir_ppo.yml base.yml $@
-./scripts/run_experiment.sh ride.yml ir_ppo.yml base.yml $@
-./scripts/run_experiment.sh revd.yml ir_ppo.yml base.yml $@
-./scripts/run_experiment.sh rnd.yml ir_ppo.yml base.yml $@
-./scripts/run_experiment.sh ngu.yml ir_ppo.yml base.yml $@
-./scripts/run_experiment.sh icm.yml ir_ppo.yml base.yml $@
-./scripts/run_experiment.sh girm.yml ir_ppo.yml base.yml $@
-./scripts/run_experiment.sh diayn.yml ir_ppo.yml base.yml $@
+./scripts/run_experiment.sh re3.yml ir_ppo.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh rise.yml ir_ppo.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh ride.yml ir_ppo.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh revd.yml ir_ppo.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh rnd.yml ir_ppo.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh ngu.yml ir_ppo.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh icm.yml ir_ppo.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh girm.yml ir_ppo.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh diayn.yml ir_ppo.yml ppo.yml base.yml $@
 ./scripts/run_experiment.sh noisy_nets_ac.yml ppo.yml base.yml $@
