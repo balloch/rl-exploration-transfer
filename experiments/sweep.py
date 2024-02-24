990f4594475eef6c56ee1283d72b14191b270bbd
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-24 13:02:14 -0500

    increasing soft file limit

--- experiments/sweep.py
+++ experiments/sweep.py
@@ -1,9 +1,13 @@
 import os
 import sys
+import resource
 
 curren_dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(curren_dir_path, os.pardir))
 sys.path.append(parent_dir_path)
+
+_, hard_limit = resource.prlimit(os.getpid(), resource.RLIMIT_NOFILE)
+resource.prlimit(os.getpid(), resource.RLIMIT_NOFILE, (hard_limit, hard_limit))
 
 import time
 import numpy as np
