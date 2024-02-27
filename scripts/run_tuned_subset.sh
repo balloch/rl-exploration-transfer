ddf6f2235a8f1cdf690900880cab835e16207251
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-27 16:12:57 -0500

    added tuned configs

---
+++ scripts/run_tuned_subset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+methods=$(echo $1 | tr "," "\n")
+for var in $methods
+do
+    command=$(cat scripts/run_all_tuned_experiments.sh | grep "$var")
+    command="${command//\$@/"${@:2}"}" 
+    eval "$command"
+done
