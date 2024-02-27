3e629d4c5c938cde08fc2a786e2a5008a5337cee
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-26 20:08:59 -0500

    create calculate metrics script

---
+++ experiments/calculate_metrics.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+curren_dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(curren_dir_path, os.pardir))
+sys.path.append(parent_dir_path)
+
+from utils.args import get_args
+from experiments.wandb_run_data import make_data_loader_parser, load_data
+
+
+def main(args):
+    df = load_data(args=args)
+    import pdb
+
+    pdb.set_trace()
+
+
+if __name__ == "__main__":
+    parser = make_data_loader_parser()
+    args = get_args(parser=parser)
+    main(args=args)
