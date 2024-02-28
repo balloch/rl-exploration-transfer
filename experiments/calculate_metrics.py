ff0c8669de6e5c8b5f88a9617c45c5fd870f1bb8
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-28 16:36:17 -0500

    plotting metrics

--- experiments/calculate_metrics.py
+++ experiments/calculate_metrics.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import pickle as pkl
 import json
+import matplotlib.pyplot as plt
 
 from utils.args import get_args
 import experiments.wandb_run_data as wrd
@@ -24,6 +25,9 @@ wrd.PULL_FROM_WANDB = False
 wrd.FILTER_UNCONVERGED_OUT = False
 WINDOW_SIZE = 50
 CONVERGENCE_REWARD_THRESHOLD = 0.9
+
+METRICS_TO_USE = None
+AGGREGATORS_TO_USE = None
 
 
 def make_parser():
@@ -43,6 +47,23 @@ def make_parser():
         type=float,
         default=CONVERGENCE_REWARD_THRESHOLD,
         help="The convergence reward threshold.",
+    )
+
+    parser.add_argument(
+        "--metrics-to-use",
+        "-mtu",
+        type=str,
+        default=METRICS_TO_USE,
+        nargs="+",
+        help="The metrics to calculate and generate plots for.",
+    )
+    parser.add_argument(
+        "--aggregators-to-use",
+        "-atu",
+        type=str,
+        default=AGGREGATORS_TO_USE,
+        nargs="+",
+        help="The aggregators to calculate and generate plots for.",
     )
 
     return parser
@@ -87,16 +108,35 @@ def aggregate_metrics(
     }
 
 
-def iq(arr: np.ndarray):
-    sorted_arr = np.sort(arr)
-    n = len(arr)
-    return sorted_arr[int(np.ceil(0.25 * n)) : int(np.floor(0.75 * n)) + 1]
+def iq(arr: np.ndarray, quartile_margin: int = 25):
+    sorted_arr = np.sort(arr, axis=-1)
+    n = sorted_arr.shape[-1]
+    return sorted_arr[
+        ...,
+        int(np.ceil(quartile_margin / 100 * n)) : int(
+            np.floor((1 - quartile_margin / 200) * n)
+        )
+        + 1,
+    ]
 
 
-def bootstrapped_sampling(arr, k=6, m=5):
+def bootstrapped_sampling(arr: np.ndarray, k: int = 6, m: int = 5):
     n = len(arr)
     idx = np.array([np.random.choice(n, size=(k,), replace=False) for _ in range(m)])
     return arr[idx]
+
+
+def calc_stats(arr: np.ndarray, ci_percentile: int = 95):
+    margin = (100 - ci_percentile) / 2
+    return {
+        "mean": arr.mean(axis=-1).mean(),
+        "std": arr.std(axis=-1).mean(),
+        "max": arr.max(axis=-1).mean(),
+        "min": arr.min(axis=-1).mean(),
+        f"ci_{ci_percentile}_lower": np.percentile(arr, margin, axis=-1).mean(),
+        f"ci_{ci_percentile}_upper": np.percentile(arr, 100 - margin, axis=-1).mean(),
+        "arr": list(arr.reshape((-1, arr.shape[-1])).mean(0)),
+    }
 
 
 class Aggregators:
@@ -104,10 +144,7 @@ class Aggregators:
     @staticmethod
     def converged(metrics: Dict[str, np.ndarray]):
         return {
-            k: {
-                "mean": metrics[k][metrics["converged"]].mean(),
-                "std": metrics[k][metrics["converged"]].std(),
-            }
+            k: calc_stats(metrics[k][metrics["converged"]])
             for k in metrics
             if k != "converged"
         }
@@ -115,10 +152,7 @@ class Aggregators:
     @staticmethod
     def converged_iq(metrics: Dict[str, np.ndarray]):
         return {
-            k: {
-                "mean": iq(metrics[k][metrics["converged"]]).mean(),
-                "std": iq(metrics[k][metrics["converged"]]).std(),
-            }
+            k: calc_stats(iq(metrics[k][metrics["converged"]]))
             for k in metrics
             if k != "converged"
         }
@@ -126,14 +160,7 @@ class Aggregators:
     @staticmethod
     def bootstrapped_converged(metrics: Dict[str, np.ndarray]):
         return {
-            k: {
-                "mean": bootstrapped_sampling(metrics[k][metrics["converged"]])
-                .mean(axis=1)
-                .mean(),
-                "std": bootstrapped_sampling(metrics[k][metrics["converged"]])
-                .std(axis=1)
-                .mean(),
-            }
+            k: calc_stats(bootstrapped_sampling(metrics[k][metrics["converged"]]))
             for k in metrics
             if k != "converged"
         }
@@ -141,39 +168,18 @@ class Aggregators:
     @staticmethod
     def bootstrapped_converged_iq(metrics: Dict[str, np.ndarray]):
         return {
-            k: {
-                "mean": iq(bootstrapped_sampling(metrics[k][metrics["converged"]]))
-                .mean(axis=1)
-                .mean(),
-                "std": iq(bootstrapped_sampling(metrics[k][metrics["converged"]]))
-                .std(axis=1)
-                .mean(),
-            }
+            k: calc_stats(iq(bootstrapped_sampling(metrics[k][metrics["converged"]])))
             for k in metrics
             if k != "converged"
         }
 
     @staticmethod
     def all(metrics: Dict[str, np.ndarray]):
-        return {
-            k: {
-                "mean": metrics[k].mean(),
-                "std": metrics[k].std(),
-            }
-            for k in metrics
-            if k != "converged"
-        }
+        return {k: calc_stats(metrics[k]) for k in metrics if k != "converged"}
 
     @staticmethod
     def all_iq(metrics: Dict[str, np.ndarray]):
-        return {
-            k: (
-                iq(metrics[k]).mean(),
-                iq(metrics[k]).std(),
-            )
-            for k in metrics
-            if k != "converged"
-        }
+        return {k: calc_stats(iq(metrics[k])) for k in metrics if k != "converged"}
 
 
 class Metrics:
@@ -208,10 +214,46 @@ class Metrics:
         """
         task_two_area_under_the_curve = task_two_rewards.mean()
 
-        return task_one_final_performance + task_two_area_under_the_curve
+        return (task_one_final_performance + task_two_area_under_the_curve) / 2
+
+
+def plot_results(results: Dict[str, Dict[str, Any]]):
+    labels = list(results.keys())
+    plots_to_generate = list(list(results.values())[0].keys())
+    metric_names = list(list(list(results.values())[0].values())[0].keys())
+    import pdb
+
+    data = [
+        [
+            [results[k][plot_name][metric]["arr"] for k in labels]
+            for plot_name in plots_to_generate
+        ]
+        for metric in metric_names
+    ]
+
+    for i in range(len(metric_names)):
+        metric = metric_names[i]
+        for j in range(len(plots_to_generate)):
+            plot_name = plots_to_generate[j]
+            plot_data = data[i][j]
+            plt.figure(figsize=(30, 15))
+            plt.boxplot(plot_data, labels=labels, vert=False, showfliers=True)
+            plt.title(f"{metric}_{plot_name}")
+            plt.savefig(f"./figures/{metric}_{plot_name}.png")
+            plt.close()
 
 
 def main(args):
+    metrics_to_use = (
+        Metrics.__dict__.keys()
+        if args.metrics_to_use is None
+        else set([k.lower() for k in args.metrics_to_use])
+    )
+    aggregators_to_use = (
+        Aggregators.__dict__.keys()
+        if args.aggregators_to_use is None
+        else set([k.lower() for k in args.aggregators_to_use])
+    )
     df = load_data(args=args)
     metrics = calculate_metrics(
         args=args,
@@ -219,7 +261,7 @@ def main(args):
         metrics_fns={
             k: v.__func__
             for k, v in Metrics.__dict__.items()
-            if isinstance(v, staticmethod)
+            if isinstance(v, staticmethod) and k.lower() in metrics_to_use
         },
     )
     results = aggregate_metrics(
@@ -227,13 +269,10 @@ def main(args):
         aggs={
             k: v.__func__
             for k, v in Aggregators.__dict__.items()
-            if isinstance(v, staticmethod)
+            if isinstance(v, staticmethod) and k.lower() in aggregators_to_use
         },
     )
-
-    import pdb
-
-    pdb.set_trace()
+    plot_results(results=results)
 
     with open("./data/metrics.pkl", "wb") as f:
         pkl.dump(metrics, file=f)
