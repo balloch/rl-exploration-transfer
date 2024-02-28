eb7c6a5ec395e9c06614c8adebe0012e9ca9b181
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-28 12:22:49 -0500

    switched to tuned configs

---
+++ scripts/run_tuned_subset2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+methods=$(echo $1 | tr "," "\n")
+for var in $methods
+do
+    command=$(cat scripts/run_all_tuned_experiments.sh | grep "$var")
+    command="${command//\$@/"${@:2} --total-time-steps 5000000 --novelty-step 2000000 --env-configs-file simple_to_lava_crossing"}" 
+    eval "$command"
+done
