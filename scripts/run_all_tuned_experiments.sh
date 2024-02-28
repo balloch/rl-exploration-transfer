858ba523ffa9c35682953661258949bb16131799
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-02-27 22:13:32 -0500

    minor fix

--- scripts/run_all_tuned_experiments.sh
+++ scripts/run_all_tuned_experiments.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 ./scripts/run_experiment.sh tuned/ppo.yml base.yml $@ --experiment-suffix _none
-./scripts/run_experiment.sh tuned/re3.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/rise.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/ride.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/revd.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/rnd.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/ngu.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/icm.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/girm.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/diayn.yml ir_ppo.yml ppo.yml base.yml $@
-./scripts/run_experiment.sh tuned/noisy_nets_ac.yml ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/re3.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/rise.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/ride.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/revd.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/rnd.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/ngu.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/icm.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/girm.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/diayn.yml ir_ppo.yml tuned/ppo.yml base.yml $@
+./scripts/run_experiment.sh tuned/noisy_nets_ac.yml tuned/ppo.yml base.yml $@
