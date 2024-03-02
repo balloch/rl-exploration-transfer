5bad8c0f0376300e1bd9bf2967dbe8f266ffc062
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-01 19:29:22 -0500

    pushed new config

--- scripts/run_tuned_subset4.sh
+++ scripts/run_tuned_subset4.sh
@@ -3,6 +3,6 @@ methods=$(echo $1 | tr "," "\n")
 for var in $methods
 do
     command=$(cat scripts/run_all_tuned_experiments.sh | grep "$var")
-    command="${command//\$@/"${@:2} --total-time-steps 7000000 --novelty-step 5000000 --env-configs-file lava_maze_hurt_to_safe"}" 
+    command="${command//\$@/"${@:2} --total-time-steps 12000000 --novelty-step 10000000 --env-configs-file lava_maze_hurt_to_safe"}" 
     eval "$command"
 done
