b6ae83b90661fcd4992fac7d00bee596f8b6ce82
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-04 12:47:28 -0500

    added save into all_results script

--- scripts/all_results.sh
+++ scripts/all_results.sh
@@ -2,3 +2,4 @@ python experiments/plot_data.py -pfw t
 python experiments/plot_data.py -pfw f --algs rnd noisy ngu rise diayn none
 python experiments/plot_data.py -pfw f --algs rnd icm girm ride none
 python experiments/calculate_metrics.py -pfw f
+./scripts/save_results.sh $1
