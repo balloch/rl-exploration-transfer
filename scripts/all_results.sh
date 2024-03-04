f3b9e2626c046ad9f64e9845f456752a3650b3d6
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-04 11:44:17 -0500

    added scripts to save results

---
+++ scripts/all_results.sh
@@ -0,0 +1,4 @@
+python experiments/plot_data.py -pfw t
+python experiments/plot_data.py -pfw f --algs rnd noisy ngu rise diayn none
+python experiments/plot_data.py -pfw f --algs rnd icm girm ride none
+python experiments/calculate_metrics.py -pfw f
---
+++ scripts/save_results.sh
@@ -0,0 +1,9 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'Please provide a name to save these results under.'
+    exit 1
+fi
+
+mkdir -p results/$1/figures
+mkdir -p results/$1/data
+mv figures/* results/$1/figures
+mv data/* results/$1/data
