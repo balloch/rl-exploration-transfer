7fe27521d74a0ac6d0c9e48f4eda93cb22859d83
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-05 16:31:06 -0500

    added cartpole sweep configs

--- scripts/run_all_cartpole_sweeps.sh
+++ scripts/run_all_cartpole_sweeps.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/ppo.yml cartpole/cartpole_sweeps/base.yml ppo.yml $@ --experiment-suffix _none
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/re3.yml cartpole/cartpole_sweeps/base.yml re3.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/rise.yml cartpole/cartpole_sweeps/base.yml rise.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/ride.yml cartpole/cartpole_sweeps/base.yml ride.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/revd.yml cartpole/cartpole_sweeps/base.yml revd.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/rnd.yml cartpole/cartpole_sweeps/base.yml rnd.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/ngu.yml cartpole/cartpole_sweeps/base.yml ngu.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/icm.yml cartpole/cartpole_sweeps/base.yml icm.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/girm.yml cartpole/cartpole_sweeps/base.yml girm.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/diayn.yml cartpole/cartpole_sweeps/base.yml diayn.yml ir_ppo.yml ppo.yml $@
-./scripts/run_sweep.sh cartpole/cartpole_sweeps/noisy_nets.yml cartpole/cartpole_sweeps/base.yml noisy_nets_ac.yml ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/re3.yml cartpole/cartpole_sweeps/base.yml re3.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/rise.yml cartpole/cartpole_sweeps/base.yml rise.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/ride.yml cartpole/cartpole_sweeps/base.yml ride.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/revd.yml cartpole/cartpole_sweeps/base.yml revd.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/rnd.yml cartpole/cartpole_sweeps/base.yml rnd.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/ngu.yml cartpole/cartpole_sweeps/base.yml ngu.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/icm.yml cartpole/cartpole_sweeps/base.yml icm.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/girm.yml cartpole/cartpole_sweeps/base.yml girm.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
+./scripts/run_sweep.sh cartpole/cartpole_sweeps/diayn.yml cartpole/cartpole_sweeps/base.yml diayn.yml ir_ppo.yml cartpole/tuned/ppo.yml $@
