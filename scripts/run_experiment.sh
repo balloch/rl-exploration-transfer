7813c54b2b278841416e058eccacb7c28dd9fc73
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-05 15:46:00 -0500

    removed conda stuff

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-eval "$(conda shell.bash hook)"
-conda activate dm_transfer
 python experiments/main.py -c $@
-conda deactivate
--- scripts/run_sweep.sh
+++ scripts/run_sweep.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-eval "$(conda shell.bash hook)"
-conda activate dm_transfer
 python experiments/sweep.py -c $@
-conda deactivate
