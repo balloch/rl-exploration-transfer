07d7e0efe58c13f6169122ad0fb551b84533cc60
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-05 15:04:26 -0500

    added setup env script

---
+++ setup_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+conda create -n dm_transfer python=3.8 -y
+conda activate dm_transfer
+export MUJOCO_GL=egl
+conda install -c "nvidia/label/cuda-11.8.0" cuda-toolkit
+conda install cmake bzip2
+python -m pip install -r dm_req.txt
+python -m pip install -e ../realworldrl_suite
+python -m pip install shimmy
+python -m pip install gymnasium
+python -m pip install stable_baselines3
+python -m pip install minigrid
+python -m pip install wandb
+python -m pip install tensorboard
+python -m pip install tqdm
+python -m pip install -e ../NovGrid
