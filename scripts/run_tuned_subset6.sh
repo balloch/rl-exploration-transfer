9c19e75dd2d8e80b1b7e95c51506e20fd4c605ca
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-06 11:46:43 -0500

    removed n-envs

--- scripts/run_tuned_subset6.sh
+++ scripts/run_tuned_subset6.sh
@@ -3,6 +3,6 @@ methods=$(echo $1 | tr "," "\n")
 for var in $methods
 do
     command=$(cat scripts/run_all_tuned_experiments.sh | grep "$var")
-    command="${command//\$@/"${@:2} --n-runs 1 --n-envs 20 --total-time-steps 9000000 --novelty-step 60000000 --env-configs-file walker_friction --wrappers FlattenObservation"}" 
+    command="${command//\$@/"${@:2} --n-runs 1 --total-time-steps 9000000 --novelty-step 60000000 --env-configs-file walker_friction --wrappers FlattenObservation"}" 
     eval "$command"
 done
