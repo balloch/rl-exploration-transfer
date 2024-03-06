2b39c42dc20f8e7fb5a9b9f9f81fe432e031e4f5
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-06 16:12:39 -0500

    added scripts

--- scripts/run_tuned_subset6.sh
+++ scripts/run_tuned_subset6.sh
@@ -3,6 +3,6 @@ methods=$(echo $1 | tr "," "\n")
 for var in $methods
 do
     command=$(cat scripts/run_all_tuned_cartpole.sh | grep "$var")
-    command="${command//\$@/"${@:2} --n-runs 1 --novelty-step 60000000 --env-configs-file walker_thigh_length --wrappers FlattenObservation"}" 
+    command="${command//\$@/"${@:2} --n-runs 1 --novelty-step 10000000 --total-time-steps 20000000 --env-configs-file walker_thigh_length --wrappers FlattenObservation"}" 
     eval "$command"
 done
---
+++ scripts/run_tuned_subset7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+methods=$(echo $1 | tr "," "\n")
+for var in $methods
+do
+    command=$(cat scripts/run_all_tuned_cartpole.sh | grep "$var")
+    command="${command//\$@/"${@:2} --n-runs 1 --novelty-step 10000000 --total-time-steps 20000000 --env-configs-file quadruped --wrappers FlattenObservation"}" 
+    eval "$command"
+done
