597c6448f32d56f3c3e4632dd7811c657ae8e0b4
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-03-07 12:44:32 -0500

    updated run configs

--- scripts/run_tuned_subset6.sh
+++ scripts/run_tuned_subset6.sh
@@ -3,6 +3,6 @@ methods=$(echo $1 | tr "," "\n")
 for var in $methods
 do
     command=$(cat scripts/run_all_tuned_cartpole.sh | grep "$var")
-    command="${command//\$@/"${@:2} --n-runs 5 --n-envs 10 --novelty-step 10000000 --total-time-steps 20000000 --env-configs-file walker_thigh_length --wrappers FlattenObservation"}" 
+    command="${command//\$@/"${@:2} --novelty-step 10000000 --total-time-steps 20000000 --env-configs-file walker_thigh_length"}" 
     eval "$command"
 done
